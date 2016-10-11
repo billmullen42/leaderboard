b8e28e6f569f94fb91755d0cffc67cc511503194
Bill Mullen <wlmullen@gmail.com>
2016-10-10 19:17:14 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+console.log("Hello World");
+
+PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function() {
+      let playerID = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if (playerID === selectedPlayer) {
+        return 'selected';
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      let playerID = this._id;
+      Session.set('selectedPlayer', playerID);
+    }
+  });
+}
+
+if (Meteor.isServer){
+  // no code
+}
+
+
